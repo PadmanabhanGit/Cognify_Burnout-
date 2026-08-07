ecc1e6c2b9c19a592f7ecf14263def5e48b1c5a6
PadmanabhanGit <padmanabhan.09k@gmail.com>
2026-08-07 14:42:33 +0530

    Remove dead code and unused local files

--- android implementation/com/simats/burnouttracker/utils/DateTimeUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.simats.burnouttracker.utils
-
-expect fun formatTimestamp(timestamp: Long): String
-expect fun formatMinutes(minutes: Int): String
-expect fun formatHours(hours: Float): String
-expect fun formatCurrentTime(): String
-expect fun formatDashboardDate(): String
